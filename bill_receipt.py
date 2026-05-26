5f06f9c6378f84bd4a8fdb8132fa0dd556fa54c1
adityajgp <adityajjnv@gmail.com>
2026-05-27 00:14:02 +0530

    Update bill_receipt.py
    Added space after asking for cost.

--- bill_receipt.py
+++ bill_receipt.py
@@ -1,7 +1,7 @@
 item = input("What is your order? ")
 item = item.title().strip()
 qty = float(input(f"How many of {item}s did you order? "))
-cost1 = input(f"How much does one {item} cost? (in your unit U)")
+cost1 = input(f"How much does one {item} cost? (in your unit U) ")
 cost2 = float(cost1.strip("$"))
 z = qty * cost2
 print("--- RECEIPT ---")
